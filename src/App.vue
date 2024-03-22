582e2ee6cdf47d3c7677b016e739ffeec98d55f8
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2024-03-22 10:34:21 +0300

    refactor: decompose the Pagination component

--- src/App.vue
+++ src/App.vue
@@ -51,28 +51,12 @@ const deleteTodo = (id) => {
             </v-list>
           </template>
           <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
-            <v-toolbar class="px-2">
-              <div class="d-flex align-center justify-center w-100">
-                <v-btn
-                  :disabled="page === 1"
-                  class="me-2"
-                  icon="mdi-arrow-left"
-                  size="small"
-                  variant="tonal"
-                  @click="prevPage"
-                />
-                <div class="mx-2 text-caption">
-                  Page {{ page }} of {{ pageCount }}
-                </div>
-                <v-btn
-                  :disabled="page === pageCount"
-                  icon="mdi-arrow-right"
-                  size="small"
-                  variant="tonal"
-                  @click="nextPage"
-                />
-              </div>
-            </v-toolbar>
+            <Pagination
+              :page="page"
+              :pageCount="pageCount"
+              :prevPage="prevPage"
+              :nextPage="nextPage"
+            />
           </template>
         </v-data-iterator>
       </v-container>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  page: { type: Number, required: true },
+  pageCount: { type: Number, required: true },
+  prevPage: { type: Function, required: true },
+  nextPage: { type: Function, required: true },
+});
+</script>
+
+<template>
+  <v-toolbar class="px-2">
+    <div class="d-flex align-center justify-center w-100">
+      <v-btn
+        :disabled="page === 1"
+        class="me-2"
+        icon="mdi-arrow-left"
+        size="small"
+        variant="tonal"
+        @click="prevPage"
+      />
+      <div class="mx-2 text-caption">Page {{ page }} of {{ pageCount }}</div>
+      <v-btn
+        :disabled="page === pageCount"
+        icon="mdi-arrow-right"
+        size="small"
+        variant="tonal"
+        @click="nextPage"
+      />
+    </div>
+  </v-toolbar>
+</template>
